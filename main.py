d11faaf2c8d418da3d280489fa8604f3deae47fa
Albert Chen <chenalbert06@gmail.com>
2021-02-13 16:36:12 -0500

    edited a few things and added sw option

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ import os
 # local imports
 from calculations import calculations
 
+calculations = calculations()
 load_dotenv()
 class Leaderboard():
     def __init__(self) -> None:
@@ -19,6 +20,10 @@ class Leaderboard():
         self.TOKEN = os.getenv('API_TOKEN')
         
         self.playerDict = {}
+        
+        self.hypixelAPI = lambda playerName: requests.get(f"https://api.hypixel.net/player?key={self.TOKEN}&name={playerName}").json()
+        
+        self.currentOptions = ["L", "BWSTAR", "SWSTAR"]
         pass
     
     def listPlayers(self):
@@ -26,7 +31,7 @@ class Leaderboard():
         playerNames = []        
         while True:
             player = input("input a player you want to add to the leaderboard (type exit to finish typing): ")
-            api = requests.get(f"https://api.hypixel.net/player?key={self.TOKEN}&name={player}").json()
+            api = self.hypixelAPI(player)
 
             if player == "exit":
                 break
@@ -36,27 +41,49 @@ class Leaderboard():
                 playerNames.append(player)
         return playerNames
     
+    def findRankings(self, allPlayers, playerDict, type):
+        levelsDict = {}
+        print(allPlayers, playerDict)
+        levelsKeys = sorted(allPlayers, key=playerDict.get, reverse=True)
+
+        for key in levelsKeys:
+            levelsDict[key] = self.playerDict[key]
+
+        currentPos = 1
+        for player in levelsDict:
+            print(f"{currentPos}. {player}: {levelsDict[player]} {type}")
+            currentPos += 1
+            
     def run(self) -> None:
         lb = input("what do you want a leaderboard for? current options are Hypixel Levels (L), Bedwars Stars (BWSTAR), and Skywars Stars (SWSTAR): ")
+        
+        if lb not in self.currentOptions:
+            print("i can't find that option")
+            return
+        
         allPlayers = self.listPlayers()
+        
         if lb == "L":
             for playerName in allPlayers:
-                hypixelAPI = requests.get(f"https://api.hypixel.net/player?key={self.TOKEN}&name={playerName}").json()
-                xp = hypixelAPI["player"]["networkExp"]
+                api = self.hypixelAPI(playerName)
+                xp = api["player"]["networkExp"]
 
                 self.playerDict[playerName] = xp
 
-            levelsDict = {}
-            levelsKeys = sorted(allPlayers, key=self.playerDict.get, reverse=True)
-
-            for key in levelsKeys:
-                levelsDict[key] = self.playerDict[key]
-
-            currentPos = 1
-            for player in levelsDict:
-                print(f"{currentPos}. {player}: {levelsDict[player]}")
-                currentPos += 1
-
+            self.findRankings(allPlayers, self.playerDict, "xp")
+                
+        if lb == "SWSTAR":
+            for playerName in allPlayers:
+                api = self.hypixelAPI(playerName)
+                swxp = api["player"]["stats"]["SkyWars"]["skywars_experience"]
+                
+                star = calculations.skywarsLevel(swxp)
+                
+                self.playerDict[playerName] = star
+            
+            self.findRankings(allPlayers, self.playerDict, "stars")
+                
+            
 if __name__ == "__main__":
     leaderboard = Leaderboard()
     
